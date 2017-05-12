266d3d2d7e65aa71bcc499ac3b2cd409db43a3b6
justinas.anatanavicius@ucll.be <justinas@Justinass-MacBook-Air.local>
2017-05-12 15:08:40 +0200

    first commi

---
+++ src/main/java/com/ucll/jobFair/JobFairApplication.java
@@ -0,0 +1,12 @@
+package com.ucll.jobFair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFairApplication.class, args);
+	}
+}
